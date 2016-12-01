64a23062c9f66e3d5aa233cffb38fb31f3c8d9d1
wllm-git <wllm.gtr@gmail.com>
2016-12-01 00:28:28 -0300

    alteração na tela de check in de aluguel.
    Signed-off-by: wllm-git <wllm.gtr@gmail.com>

--- AlugaGamesApresentacao/src/com/alugagames/controller/aluguel/AluguelCheckInBean.java
+++ AlugaGamesApresentacao/src/com/alugagames/controller/aluguel/AluguelCheckInBean.java
@@ -1,7 +1,11 @@
 package com.alugagames.controller.aluguel;
 
+import java.util.List;
+
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+
+import com.alugagames.util.FacesUtil;
 
 import alugagames.aplicacao.AluguelAplicacao;
 import alugagames.core.alugueis.Aluguel;
@@ -12,8 +16,30 @@ public class AluguelCheckInBean {
 	private Aluguel aluguel;
 	private AluguelAplicacao aluguelAplicacao;
 	
+	public AluguelCheckInBean(Aluguel aluguel){
+		this.aluguel = aluguel;
+		
+		aluguelAplicacao = new AluguelAplicacao();
+	}
 	
-	
+	public void finalizarAluguel(){
+		
+		
+		List<String> retorno = aluguelAplicacao.finalizarAluguel(aluguel);
+
+		if (!retorno.isEmpty()) {
+
+			for (String erro : retorno) {
+				FacesUtil.addErrorMessage(erro);
+			}
+
+			return;
+		}
+
+		FacesUtil.addInfoMessage("Cliente cadastrado com sucesso!");
+		
+		//TODO falta redirecionar para outra tela.
+	}
 	
 	public Aluguel getAluguel() {
 		return aluguel;
