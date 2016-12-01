bce1f94bc24df85f3e2aa434d0e73aca770b3156
wllm-git <wllm.gtr@gmail.com>
2016-11-30 22:56:15 -0300

    adicionado as classes bean de CheckIn e CheckOut de aluguel.
    Signed-off-by: wllm-git <wllm.gtr@gmail.com>

---
+++ AlugaGamesApresentacao/src/com/alugagames/controller/aluguel/AluguelCheckInBean.java
@@ -0,0 +1,24 @@
+package com.alugagames.controller.aluguel;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+import alugagames.aplicacao.AluguelAplicacao;
+import alugagames.core.alugueis.Aluguel;
+
+@ManagedBean
+@ViewScoped
+public class AluguelCheckInBean {
+	private Aluguel aluguel;
+	private AluguelAplicacao aluguelAplicacao;
+	
+	
+	
+	
+	public Aluguel getAluguel() {
+		return aluguel;
+	}
+	public void setAluguel(Aluguel aluguel) {
+		this.aluguel = aluguel;
+	}
+}
